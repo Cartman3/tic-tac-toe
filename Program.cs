c18bf5b1be1ddbc0b049b8cc0a971574a46c5c4f
Carter Brown <carter.b@earthlink.net>
2022-07-08 10:24:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace game
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is in C#");
+
+            Console.WriteLine("What is your favorite color? ");
+            string color = Console.ReadLine();
+            Console.WriteLine(color);
+        }
+    }
+}
